ddb4b53af2c934e9535250323dfe7d03cac71014
deijahmonay <deijah@shack15.com>
2024-09-30 20:33:39 -0700

    create model directory, crete car model

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ mongoose.connect(process.env.MONGODB_URI);
 
 
 mongoose.connection.on("connected", () => {
-  console.log(`Connected to MongoDB ${mongoose.connection}.`);
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
 
 
